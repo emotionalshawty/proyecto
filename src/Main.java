72592d83355363e1855ef9743dac3f8240309696
emotionalshawtea <matrixomfg@gmail.com>
2025-03-05 19:30:45 +0100

    2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        method1();
+        method2();
+        method3();
+        method4();
+    }
+    private static void method1(){
+
+    }
+    private static void method2(){
+
+    }
+    private static void method3(){
+
+    }
+    private static void method4(){
+
     }
 }
